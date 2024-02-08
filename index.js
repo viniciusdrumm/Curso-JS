3b588f99e8c67c80ae5bf9375b935c14982a96c1
Vinicius Drumm <dev.viniciusdrumm@gmail.com>
2024-02-08 20:37:00 -0300

    exercício complementar aula 4

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-var saudacao = 'Seja bem-vindo, '
-var nome = prompt('Digite o seu nome');
-console.log(saudacao + nome + "!");
-
-var anoAtual = 2024;
-var anoNascimento = prompt('Insira seu ano de nascimento');
-
-var idade = anoAtual - anoNascimento;
-
-console.log('Você tem ' + idade + ' anos.')
+function checkMultiplos(num1, num2) {
+    // Verificar se algum dos números é 0, pois a divisão por 0 é indefinida.
+    if (num1 === 0 || num2 === 0) {
+      console.log("Um dos números é 0, não é possível determinar a multiplicidade.");
+      return;
+    }
+  
+    if (num1 % num2 === 0 || num2 % num1 === 0) {
+      console.log("Os números são múltiplos.");
+    } else {
+      console.log("Os números não são múltiplos.");
+    }
+  }
+  
+  // Exemplo Usado:
+  checkMultiplos(12, 3);
+  checkMultiplos(5, 17);
+  checkMultiplos(29, 15);
+  checkMultiplos(0, 5);
